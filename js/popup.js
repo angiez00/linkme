c0d9413db1761562356d8960a9d7c53731ee2d5b
Ryan Jadhav <ryan.jadhav@gmail.com>
2012-07-29 17:32:39 -0700

    printing out all the groups per user

--- js/popup.js
+++ js/popup.js
@@ -1,38 +1,22 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-// 
+(function() {
+    if (localStorage.accessToken) {
+        var graphUrl = "https://graph.facebook.com/me/groups?" + localStorage.accessToken + "&callback=displayData";
+        console.log(graphUrl);
 
-var req = new XMLHttpRequest();
-req.open(
-    "GET",
-    "http://api.flickr.com/services/rest/?" +
-        "method=flickr.photos.search&" +
-        "api_key=90485e931f687a9b9c2a66bf58a3861a&" +
-        "text=hello%20world&" +
-        "safe_search=1&" +  // 1 is "safe"
-        "content_type=1&" +  // 1 is "photos only"
-        "sort=relevance&" +  // another good one is "interestingness-desc"
-        "per_page=20",
-    true);
-req.onload = showPhotos;
-req.send(null);
+        var script = document.createElement("script");
+        script.src = graphUrl;
+        document.body.appendChild(script);
 
-function showPhotos() {
-  var photos = req.responseXML.getElementsByTagName("photo");
+        displayData = function(data) {
+            console.log(data);
+            fbGroupData = data;
 
-  for (var i = 0, photo; photo = photos[i]; i++) {
-    var img = document.createElement("image");
-    img.src = constructImageURL(photo);
-    document.body.appendChild(img);
-  }
-}
+            $.each(fbGroupData.data, function() {
+                $('body').append('<p>' + this.name + '</p>');
+            });
 
-// See: http://www.flickr.com/services/api/misc.urls.html
-function constructImageURL(photo) {
-  return "http://farm" + photo.getAttribute("farm") +
-      ".static.flickr.com/" + photo.getAttribute("server") +
-      "/" + photo.getAttribute("id") +
-      "_" + photo.getAttribute("secret") +
-      "_s.jpg";
-}
+
+        };
+    }
+
+})();
