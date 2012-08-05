c045b118f177afc8bd36a32d712daee8a90c09bd
Ryan Jadhav <ryan.jadhav@gmail.com>
2012-08-04 23:06:47 -0700

    adding inline script for facebook

--- js/popup.js
+++ js/popup.js
@@ -1,33 +1,23 @@
 (function() {
     if (localStorage.accessToken) {
-        // var graphUrl = "https://graph.facebook.com/me/groups?" + localStorage.accessToken;
+        var graphUrl = "https://graph.facebook.com/me/groups?" + localStorage.accessToken + "&callback=displayData";
+        console.log(graphUrl);
 
-        // var graphUrl = "https://graph.facebook.com/17505397/groups",
-        //     accessToken = 'AAACEdEose0cBAMMRGxTJZAZAznK7VIDGx4ndMCvEhIH3yTXJNxnGeNziv4uQbJ2PZCk9bdRl1nKanOFSKySWEgTnenZBWC3AkR0YTekZB5QZDZD';
-        // console.log(graphUrl);
+        var script = document.createElement("script");
+        script.src = graphUrl;
+        $('body').append(script);
 
-        // $.getJSON(graphUrl,
-        // {
-        //     accessToken: accessToken
-        // },
-        // function(data) {
-        //     console.log(data);
-        //     fbGroupData = data;
-        //     if (fbGroupData.data) {
-        //         $.each(fbGroupData.data, function() {
-        //             $('body').append('<p>' + this.name + '</p>');
-        //         });
-        //     } else {
-        //         $('body').append('<p> Something went wrong with facebook. </p>');
-        //     }
-        // });
-
-        var fburl = "http://graph.facebook.com/naftali.lubin";
-
-        $.get(fburl, function(data){
+        displayData = function(data) {
             console.log(data);
-            alert(data.name);
-        });
+            fbGroupData = data;
+            if (fbGroupData.data) {
+                $.each(fbGroupData.data, function() {
+                    $('body').append('<p>' + this.name + '</p>');
+                });
+            } else {
+                $('body').append('<p> Something went wrong with facebook. </p>');
+            }
+        };
     }
 
 })();
